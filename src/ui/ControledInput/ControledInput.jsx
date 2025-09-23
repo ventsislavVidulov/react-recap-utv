ba3736eb873012dea2d7bc9e4a4b2b4c9e7b3459
ventsislavVidulov <xcen3k@gmail.com>
2025-09-23 18:24:29 +0300

    move  ControlledInput to ui dir

---
+++ src/ui/ControledInput/ControledInput.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+import styles from './ControlledInput.module.css'
+
+const ControledInput = ({ handler, debounceInterval, placeholder}) => {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInput = (e) => {
+        let timerId = null;
+        //not working properly, the timerId is redeclared to null on every invoke and 
+        // setTimeout cann't be reseted
+        setInputValue(e.target.value);
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            handler(e.target.value);
+        }, debounceInterval);
+    }
+
+    return (
+            <input className={styles.controlledTextInput} type="text" name="" id="" value={inputValue} placeholder={placeholder} onChange={e => handleInput(e)} />
+    )
+}
+
+export default ControledInput;
