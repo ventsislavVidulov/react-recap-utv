83e43f550e4c045b0349e99ba2e6579c33452e1e
ventsislavVidulov <xcen3k@gmail.com>
2025-09-23 19:04:13 +0300

    made controlled input to be resetable via signal

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import MyButton from "./ui/MyButton/MyButton";
 
 function App() {
   const [postTitle, setPostTitle] = useState('');
+  const [postTitleResetSignal, setPostTitleResetSignal] = useState(false);
+  const [postDescriptionResetSignal, setPostDescriptionResetSignal] = useState(false);
   const [postDescription, setPostDescription] = useState('');
   const [posts, setPosts] = useState(
     [
@@ -38,16 +40,16 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPosts([...posts, { id: posts[posts.length - 1].id + 1, title: postTitle, description: postDescription}]);
-    setPostTitle('');
-    setPostDescription('');
+    setPosts([...posts, { id: posts[posts.length - 1].id + 1, title: postTitle, description: postDescription }]);
+    setPostTitleResetSignal(state => !state);
+    setPostDescriptionResetSignal(state => !state);
   }
 
   return (
     <div className='app'>
       <form action="">
-        <ControledInput placeholder="Title" value={postTitle} handler={titleHandler} debounceInterval="1000" />
-        <ControledInput placeholder="Description" value={postDescription} handler={descriptionHandler} debounceInterval="1000" />
+        <ControledInput placeholder="Title" value={postTitle} handler={titleHandler} debounceInterval="1000" resetSignal={postTitleResetSignal} />
+        <ControledInput placeholder="Description" value={postDescription} handler={descriptionHandler} debounceInterval="1000" resetSignal={postDescriptionResetSignal} />
         <MyButton handler={handleSubmit}>Create post</MyButton>
       </form>
       <PostList posts={posts} listTitle="Posts" />
--- src/ui/ControledInput/ControledInput.jsx
+++ src/ui/ControledInput/ControledInput.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from './ControlledInput.module.css'
 
-const ControledInput = ({ handler, debounceInterval, placeholder}) => {
+const ControledInput = ({ handler, debounceInterval, placeholder, resetSignal}) => {
     const [inputValue, setInputValue] = useState('');
 
     const handleInput = (e) => {
         let timerId = null;
         //not working properly, the timerId is redeclared to null on every invoke and 
-        // setTimeout cann't be reseted
+        //setTimeout cann't be reseted
         setInputValue(e.target.value);
         clearTimeout(timerId);
         timerId = setTimeout(() => {
@@ -16,8 +16,10 @@ const ControledInput = ({ handler, debounceInterval, placeholder}) => {
         }, debounceInterval);
     }
 
+    useEffect(() => setInputValue(''), [resetSignal]);
+
     return (
-            <input className={styles.controlledTextInput} type="text" name="" id="" value={inputValue} placeholder={placeholder} onChange={e => handleInput(e)} />
+        <input className={styles.controlledTextInput} type="text" name="" id="" value={inputValue} placeholder={placeholder} onChange={e => handleInput(e)} />
     )
 }
 
