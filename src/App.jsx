a3e15810a9d9a6647eea9a9192b719ad222380cc
ventsislavVidulov <xcen3k@gmail.com>
2025-09-23 14:03:48 +0300

    implement basic useState logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react'
 
 function App() {
 
+  const [likes, setLikes] = useState({count: 0});
+
+  const handleIncrement = (likes) => {
+    return {
+      count: likes.count + 1
+    }
+  }
+
+  const handleDecrement = (likes) => {
+    return {
+      count: likes.count -1
+    }
+  }
+
   return (
     <>
-      <div>
-      APP WORKS!
-      </div>
+      <h1>{likes.count}</h1>
+      <button onClick={() => setLikes(handleIncrement(likes))}>Increment</button>
+      <button onClick={() => setLikes(handleDecrement(likes))}>Decrement</button>
     </>
   )
 }
