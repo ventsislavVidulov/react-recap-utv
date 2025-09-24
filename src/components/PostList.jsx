440ca7b199f7da1df41eb8ddd5706295b9bf60dd
ventsislavVidulov <xcen3k@gmail.com>
2025-09-24 12:57:34 +0300

    add conditional renderin in post list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,13 +4,14 @@ const PostList = (props) => {
     return (
         <div className='app'>
             <h1 style={{ textAlign: "center" }}>{props.listTitle}</h1>
-            {
-                props.posts.map(p =>
-                    <PostItem key={p.id} id={p.id} title={p.title} description={p.description} deletePostHandler={props.deletePostHandler}/>
+            {props.posts.length > 0
+                ? props.posts.map(p =>
+                    <PostItem key={p.id} id={p.id} title={p.title} description={p.description} deletePostHandler={props.deletePostHandler} />
                 )
+                : <h1 style ={{textAlign: "center"}}>There are no posts</h1>
             }
         </ div>
     )
 }
- 
+
 export default PostList;
