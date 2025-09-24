ce0026d93433868b161d9ee7f23eae1c5b1bb13e
ventsislavVidulov <xcen3k@gmail.com>
2025-09-24 12:58:59 +0300

    add sort logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./styles/App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./ui/PostForm/PostForm";
+import MySelect from "./ui/MySelect/MySelect";
 
 function App() {
   const [posts, setPosts] = useState(
@@ -24,6 +25,18 @@ function App() {
 
     ]
   );
+  const [sortObject, setSortObject] = useState({ accendingDescending: 'accending', textContentSortCriterion: 'id' })
+  const [sortedPosts, setSortedPosts] = useState([])
+
+  useEffect(() => {
+    if (sortObject.textContentSortCriterion === 'id') {
+      setSortedPosts([...posts.sort((a, b) => sortObject.accendingDescending === 'accending' ? a.id - b.id : b.id - a.id)])
+    } else  if (sortObject.textContentSortCriterion === 'title') {
+      setSortedPosts([...posts.sort((a, b) => sortObject.accendingDescending === 'accending' ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title))])
+    } else  if (sortObject.textContentSortCriterion === 'description') {
+      setSortedPosts([...posts.sort((a, b) => sortObject.accendingDescending === 'accending' ? a.description.localeCompare(b.description) : b.description.localeCompare(a.description))])
+    }
+  }, [sortObject, posts])
 
   const addPostHandler = (posts) => {
     setPosts(posts);
@@ -33,10 +46,43 @@ function App() {
     setPosts(posts.filter(p => p.id !== postId))
   }
 
+  const textContentSortHandler = (e) => {
+    setSortObject({ ...sortObject, textContentSortCriterion: e.target.value });
+  }
+
+  const accendingDescendingHandler = (e) => {
+    setSortObject({ ...sortObject, accendingDescending: e.target.value });
+  }
+
+    const sortChriterionHandler= (e) => {
+    setSortObject({ ...sortObject, sortChriterionHandler: e.target.value });
+  }
+
   return (
     <div className='app'>
-      <PostList posts={posts} deletePostHandler={deletePostHandler} listTitle="Posts" />
+      <MySelect
+        options={
+          [
+            { value: "id", textContent: "Sort by id" },
+            { value: "title", textContent: "Sort by title" },
+            { value: "description", textContent: "Sort by description" },
+          ]}
+        // defaultValue="Select sort method"
+        onChange={textContentSortHandler}
+        value={sortObject.textContentSortCriterion}
+      />
+      <MySelect
+        options={
+          [
+            { value: "accending", textContent: "Accending" },
+            { value: "descending", textContent: "Descending" },
+          ]}
+        // defaultValue="Accending/Descending"
+        onChange={accendingDescendingHandler}
+        value={sortObject.accendingDescending}
+      />
       <PostForm posts={posts} addPostHandler={addPostHandler} />
+      <PostList posts={sortedPosts} deletePostHandler={deletePostHandler} listTitle="Posts" />
     </ div>
   )
 }
