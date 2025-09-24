41b0528e912e107ea43e7866f282a5dae7e43b2c
ventsislavVidulov <xcen3k@gmail.com>
2025-09-24 12:57:07 +0300

    remove debounce logic

--- src/ui/ControledInput/ControledInput.jsx
+++ src/ui/ControledInput/ControledInput.jsx
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 
 import styles from './ControlledInput.module.css'
 
-const ControledInput = ({ handler, debounceInterval, placeholder, resetSignal}) => {
+const ControledInput = ({ handler, debounceInterval, placeholder, resetSignal }) => {
     const [inputValue, setInputValue] = useState('');
 
     const handleInput = (e) => {
-        let timerId = null;
-        //not working properly, the timerId is redeclared to null on every invoke and 
+        // let timerId = null;
+        //debounce not working properly, the timerId is redeclared to null on every invoke and 
         //setTimeout cann't be reseted
         setInputValue(e.target.value);
-        clearTimeout(timerId);
-        timerId = setTimeout(() => {
-            handler(e.target.value);
-        }, debounceInterval);
+        // clearTimeout(timerId);
+        // timerId = setTimeout(() => {
+        handler(e.target.value);
+        // }, debounceInterval);
     }
 
     useEffect(() => setInputValue(''), [resetSignal]);
